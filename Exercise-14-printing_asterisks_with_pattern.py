b49dabe18608ad38e29d4d918154fba54dd16a36
unknown <miggy.payumo26@gmail.com>
2024-01-27 00:04:28 +0800

    Code finalized and labeled

--- Exercise-14-printing_asterisks_with_pattern.py
+++ Exercise-14-printing_asterisks_with_pattern.py
@@ -7,7 +7,11 @@
 # * *
 # *
 
+# loop for asterisks going across at the top
 for asterisk_across in range(6, 0, -1):
+# loop for asterisks going down at the side
     for asterisk_down in range(0, asterisk_across -1):
+# for printing the asterisks
         print("*", end=' ')
+# for adding pattern to the asterisks
     print(" ")
